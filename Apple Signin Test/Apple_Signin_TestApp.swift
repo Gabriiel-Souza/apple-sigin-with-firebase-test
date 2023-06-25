fd9319407693ebc31f0e9230f7339cce747f98f4
Gabriel Souza <gabriiell.souza@outlook.com>
2023-06-25 18:44:33 -0300

    Initial commit

---
+++ Apple Signin Test/Apple_Signin_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Signin_TestApp.swift
+//  Apple Signin Test
+//
+//  Created by Gabriel Souza de Araujo on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Signin_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
